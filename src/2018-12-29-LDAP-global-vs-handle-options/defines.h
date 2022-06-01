204b370036bd5ba57bfca732060c6655aaf1ca86
Yang Yang <yyang.pplus@gmail.com>
2022-05-31 23:16:23 -0700

    Posts: Add set-openldap-debug-level

--- src/2018-12-29-LDAP-global-vs-handle-options/defines.h
+++ src/2018-12-29-LDAP-global-vs-handle-options/defines.h
@@ -5,7 +5,9 @@
 #include <ldap.h>
 
 
+#define LDAP_DEBUG_ANY (-1)
+
 const std::string URI = "ldap://";
 const std::string BASE = "dc=yyang-pplus,dc=github,dc=io";
 constexpr auto SCOPE = LDAP_SCOPE_BASE;
-constexpr auto DEBUG_LEVEL = -1;
+constexpr auto DEBUG_LEVEL = LDAP_DEBUG_ANY;
