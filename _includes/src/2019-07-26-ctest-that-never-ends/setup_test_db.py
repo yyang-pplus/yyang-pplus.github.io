22004ebe49f7d81788ae577960ec9e9116dc783b
Yang Yang <yyang.pplus@gmail.com>
2022-06-08 00:08:08 -0700

    CMake: Remove minimum requirement from subprojects

--- _includes/src/2019-07-26-ctest-that-never-ends/setup_test_db.py
+++ _includes/src/2019-07-26-ctest-that-never-ends/setup_test_db.py
@@ -4,6 +4,7 @@ import subprocess
 
 
 DBFARM_PATH = "/tmp/dbfarm"
+subprocess.call(["monetdbd", "create", DBFARM_PATH])
 subprocess.call(
     ["monetdbd", "start", DBFARM_PATH],
     stdin=subprocess.PIPE,
