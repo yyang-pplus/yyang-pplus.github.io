1acceb6cc1ddb0b4c95d881de544d1e7fb1f6d29
Yang Yang <yyang.pplus@gmail.com>
2022-06-09 22:32:47 -0700

    Post: Finished migration

--- _includes/src/2019-07-26-ctest-that-never-ends/dependencies.sh
+++ _includes/src/2019-07-26-ctest-that-never-ends/dependencies.sh
@@ -7,16 +7,30 @@ set -e
 #               https://www.monetdb.org/downloads/deb/
 ##
 
-Suite=$(lsb_release -cs)
+SUITE=$(lsb_release -cs)
 SOURCE_LIST="/etc/apt/sources.list.d/monetdb.list"
 
 cat << EOF | sudo tee $SOURCE_LIST
-deb https://dev.monetdb.org/downloads/deb/ $Suite monetdb
-deb-src https://dev.monetdb.org/downloads/deb/ $Suite monetdb
+deb https://dev.monetdb.org/downloads/deb/ $SUITE monetdb
+deb-src https://dev.monetdb.org/downloads/deb/ $SUITE monetdb
 EOF
 
 wget --output-document=- https://www.monetdb.org/downloads/MonetDB-GPG-KEY | sudo apt-key add -
 
 sudo apt update
 
-sudo apt --yes install monetdb5-sql monetdb-client
+VERSION="11.35.19"
+
+temp_dir="/tmp/$(date +%s)"
+mkdir $temp_dir
+
+pushd $temp_dir
+SELECTED_PACKAGES=(monetdb5-server monetdb5-sql monetdb-client)
+for a_package in ${SELECTED_PACKAGES[@]}; do
+    PACKAGE_NAME="${a_package}_${VERSION}_amd64.deb"
+    wget https://www.monetdb.org/downloads/deb/archive/$SUITE/$PACKAGE_NAME
+    sudo apt --yes install "./$PACKAGE_NAME"
+done
+popd
+
+rm -rf "$temp_dir"
