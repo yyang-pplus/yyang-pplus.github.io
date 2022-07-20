43b97e6118e42bb01839a65424d0699693a90ce0
Yang Yang <yyang.pplus@gmail.com>
2022-07-19 23:34:07 -0700

    Scripts: Add test case testInSrcFilenameMatchRealFilename() to format_tests

--- scripts/format_tests.sh
+++ scripts/format_tests.sh
@@ -16,4 +16,29 @@ testNoUnexpectedKeywordsOrder() {
     QuietRun popd
 }
 
+testInSrcFilenameMatchRealFilename() {
+    PROJECT_ROOT_DIR=$(GetProjectRootDir)
+    QuietRun pushd "$PROJECT_ROOT_DIR"
+
+    ALL_SRC_FILES=($(git ls-files -- '*.cpp' '*.hpp' '*.py' '*.sh'))
+    for src_file in "${ALL_SRC_FILES[@]}"; do
+        filename=$(basename "$src_file")
+        extension="${filename##*.}"
+
+        if [ "$extension" = "cpp" ] || [ "$extension" = "hpp" ]; then
+            first_line=$(head -1 "$src_file")
+            comment_char='//'
+        else
+            first_line=$(sed '3q;d' "$src_file")
+            comment_char='#'
+        fi
+
+        if [[ $first_line == $comment_char* ]] && [[ $first_line == *$extension ]]; then
+            assertEquals "$first_line" "$comment_char $filename"
+        fi
+    done
+
+    QuietRun popd
+}
+
 source /usr/bin/shunit2
